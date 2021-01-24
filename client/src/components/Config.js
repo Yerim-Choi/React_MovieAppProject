c11de116db7ef53341a7e97a9914a996438cc423
Yerim Choi_ <67423172+Yerim-Choi@users.noreply.github.com>
2021-01-25 01:24:07 +0900

    MovieApp 어플리케이션 작업 완료

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,9 @@
+import { connect } from "mongoose";
+
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = '48b065dff2c0247b83d68ca5e888d92c';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
